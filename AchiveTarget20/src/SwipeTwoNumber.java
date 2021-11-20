d84474549b52a422fbd5aac55822831c187c1098
shara9 <shanurana1724@gmail.com>
2021-11-20 20:27:00 +0530

    Swipe two number without third veriable

---
+++ AchiveTarget20/src/SwipeTwoNumber.java
@@ -0,0 +1,14 @@
+
+public class SwipeTwoNumber {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=3;
+		a=a+b;
+		b=a-3;
+		a=a-b;
+		System.out.println("a="+ a+ "\nb="+b);
+
+	}
+
+}
