ff7525ccdf9b7199e332c1bd32c93bf89e6420f5
shara9 <shanurana1724@gmail.com>
2021-11-21 16:34:14 +0530

    method overloading

---
+++ AchiveTarget20/src/Overloading.java
@@ -0,0 +1,19 @@
+
+public class Overloading {
+
+	public static void main(String[] args) {
+	
+				Add a=new Add();
+				System.out.println(a.add(2,4));
+				System.out.println(a.add(5, 5,7));
+			}}
+			 class Add{
+				public int add(int a, int b) {
+				return(a+b);
+				}
+				public int add(int a,int b, int c)
+				{
+					return(a+b+c);
+				}}
+
+
