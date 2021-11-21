b905d2d528c4df607177d9dce35d701c313fcced
shara9 <shanurana1724@gmail.com>
2021-11-21 11:43:53 +0530

    reverse number

---
+++ AchiveTarget20/src/ReverseNumberOfArray.java
@@ -0,0 +1,12 @@
+
+public class ReverseNumberOfArray {
+		public static void main(String[] ar)
+		{
+			int[] a = {7,8,4,6,2};
+			for(int i=a.length-1; i>=0; i--)
+			{
+				System.out.println(a[i]);
+			}
+		}
+	
+}
