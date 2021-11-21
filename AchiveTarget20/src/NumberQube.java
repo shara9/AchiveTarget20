f42894c8df39d23d6f0817e13b255fd98bf4510f
shara9 <shanurana1724@gmail.com>
2021-11-21 14:23:26 +0530

    qube of number

---
+++ AchiveTarget20/src/NumberQube.java
@@ -0,0 +1,11 @@
+
+public class NumberQube {
+		public static void main(String[] args) {
+			for (int i=0;i<=6;i++)
+			{
+					for (int j=0;j<i;j++) 
+					System.out.print(i*j +" "); 
+					System.out.println();
+			}
+		}
+}
